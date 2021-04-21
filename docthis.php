f8a3e60d02969c8c7933d4644f318e3e5286cdf6
Fernando Filho <fernandofilho@MacBook-Pro-de-Fernando.local>
2021-04-21 13:03:03 -0300

    inclusao do estilo e menu lateral

--- docthis.php
+++ docthis.php
@@ -21,3 +21,26 @@
  * License:           GPL v2 or later
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  */
+
+
+
+
+add_action( 'admin_menu', 'meu_menu' );
+
+function meu_menu() {
+    add_menu_page(
+        //titulo na aba
+        'docthis',
+        //titulo no menu lateral
+        'Documentos',
+        //localização do plugin
+        'manage_options',
+        //titulo no final do link
+        'doc-this',
+        //página onde se encontra o conteudo
+        'wporg_options_page_html',
+        plugin_dir_url(__FILE__) . 'images/icon.png',
+        20
+    );
+}
+

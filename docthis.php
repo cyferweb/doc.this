cce4f95e6673b84030f638901038bee6c646e61e
Fernando Filho <fernandofilho@MacBook-Pro-de-Fernando.local>
2021-04-21 11:14:42 -0300

    ïnserindo informacoes iniciais sobre o plugin

---
+++ docthis.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Plugin Name
+ *
+ * @package           DOC.this
+ * @author            Fernando Filho
+ * @copyright         2021 cyfer development
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       DOC.this
+ * Plugin URI:        https://cyfer.com.br/
+ * Description:       Plugin simples para documentação no painel de controle de plugins, temas e tudo que seja necessário documentar
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Fernando Filho
+ * Author URI:        https://cyfer.com.br/
+ * Text Domain:       doc-this
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
